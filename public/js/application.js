bd1a1dafe8b4565797ceeec30d5f1e9cef597e79
steph t. zhang <stephtzhang@gmail.com>
2014-10-01 13:19:28 -0400

    add functionality to append user-determined num of animals

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,11 @@
 $(function() {
+  $('#game_info > input[type=button]').click(function(event) {
+    tigers = $('#tiger').val();
+    deer = $('#deer').val();
+    trees = $('#tree').val();
 
+    game = new Game(tigers, deer, trees);
+  })
 });
 
 
--- public/js/game.js
+++ public/js/game.js
@@ -1,12 +1,35 @@
-function Game() {
-  this.$arena = $('#arena');
-  // this.tigers = [new Tiger(this.$arena)];
-  // this.deer = [new Deer(this.$arena)];
-  // this.tree = [new Tree(this.$arena)];
+// in Game class or in doc ready
+// setInterval(game.process.bind(game), 20); // 50 FPS
+function Game(tigerNum, deerNum, treeNum) {
+  this.$world = $('#world');
+  this.tigers = this.createBeings(Tiger, tigerNum);
+  this.deer = this.createBeings(Deer, deerNum);
+  this.tree = this.createBeings(Tree, treeNum);
 }
 
-$(document).ready(function() {
-  game = new Game();
+Game.prototype.createBeings = function(beingClass, beingNum) {
+  var beings = []
+  for(var i = 0; i < beingNum; i++) {
+      beings.push(new beingClass(this.$world));
+  }
+  return beings;
+}
 
-  // setInterval(game.process.bind(game), 20); // 50 FPS
-});
+function Tiger($world) {
+  this.$world = $world;
+  this.$html = $("<div class='tiger'></div>");
+  this.$world.append(this.$html);
+}
+
+function Deer($world) {
+  this.$world = $world;
+  this.$html = $("<div class='deer'></div>");
+  this.$world.append(this.$html);
+
+}
+
+function Tree($world) {
+  this.$world = $world;
+  this.$html = $("<div class='tree'></div>");
+  this.$world.append(this.$html);
+}
