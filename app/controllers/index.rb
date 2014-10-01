1baeac85abbed353446163464dc6b754fbfa6ed6
steph t. zhang <stephtzhang@gmail.com>
2014-10-01 11:31:13 -0400

    add routes for user signup

---
+++ app/controllers/index.rb
@@ -0,0 +1,38 @@
+enable :sessions
+use Rack::Flash
+
+get '/' do
+  erb :index
+end
+
+get '/login' do
+  erb :"user/login_signup"
+end
+
+get '/logout' do
+  session.clear
+  erb :index
+end
+
+post '/user' do
+  user = User.create(params[:user_info])
+  if user.valid?
+    session[:user_id] = user.id
+    redirect "/"
+  else
+    flash[:new_account_error] = user.errors.messages
+    redirect '/login'
+  end
+end
+
+post '/login' do
+  user = User.find_by(name: params[:name]).try(:authenticate, params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect "/"
+  else
+    flash[:sign_in_error] = "Login password combination incorrect"
+    redirect '/login'
+  end
+end
+
