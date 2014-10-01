92a842cb2f92b4f300f33e9ca849ffb28cab91bd
steph t. zhang <stephtzhang@gmail.com>
2014-10-01 10:55:18 -0400

    add top_scores method

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,13 @@
 class Game < ActiveRecord::Base
   belongs_to :user
+
+  # call Game.top_scores(5) for 5 top scores
+  # is the game responsible for this?
+  def self.top_scores(num)
+    top_scores = self.order(score: :desc).limit(num)
+    top_scores.map do |game|
+      {name: game.user.name, score: game.score}
+    end
+  end
+
 end
